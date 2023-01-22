9b997dfe74b3fe11481d55c2a99d086c292b6176
Arnab Mitra <mitraarnab.2013@gmail.com>
2023-01-22 19:01:36 +0530

    Initial Commit

---
+++ src/main/java/com/example/app/MainController.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root (Model model){
+        model.addAttribute("test1", "Hello User");
+        return "mainTemplate";
+    }
+}
